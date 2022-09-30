9a28681a7117387966f4c7269bf8346f83cd7871
olmartin <olmartin@student.42lausanne.ch>
2022-09-30 11:33:20 +0200

    ex02 en cours

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,11 +1,38 @@
-
+#include <iostream>
 #include "Account.hpp"
 
-void	Account::Account( int deposit) : _amount(deposit) {
+Account::Account( int initial_deposit) : _amount(initial_deposit) {
 
-	std::cout << "timestamp" << " " << index: << "1" << ";";
-	std::cout << "amount: << deposit << ";created" ;
+	this->_accountIndex = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	std::cout << "timestamp " << "index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount << ";created" << std::endl;
 
+	return;
+}
+
+Account::~Account( void) {
+
+	return;
+}
+/*
+int	Account::getNbAccounts( void ) {
+	return Account::_nbAccounts;
+}
+*/
+void	Account::makeDeposit( int deposit ) {
+
+	int		p_amount;
+
+	p_amount = this->_amount;
+	this->_amount += deposit;
+	this->_nbDeposits += 1;
+//	t::_totalNbDeposits += 1;
+	std::cout << "timestamp " << "index:" << this->_nbDeposits << ";";
+	std::cout << "p_amount:" << p_amount << ";deposit:" << deposit;
+	std::cout << ":amount:" << this->_amount << ";nb_deposits:";
+	std::cout << this->_nbDeposits;
 	return;
 }
 
--- ex02/main_my.cpp
+++ ex02/main_my.cpp
@@ -4,6 +4,9 @@ int main (void)
 {
 
 	Account	acc(42);
+	acc.makeDeposit(5);
+	acc.makeDeposit(3);
+
 
 	return (1);
 }
